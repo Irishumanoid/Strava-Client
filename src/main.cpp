89421ea3f829b1081d435aa9d0f082c782c9aa53
Irishumanoid <95321751+Irishumanoid@users.noreply.github.com>
2025-09-07 16:33:01 -0700

    parse all athlete activites but exceeded api rate limit mb

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 #include <string>
 #include <format>
 #include <optional>
+#include <map>
 
 #include <utils.h>
 
@@ -29,23 +30,58 @@ std::optional<json> makeRequest(httplib::Client& client, httplib::Headers& heade
     return {};
 }
 
-int main(int, char**){
-    plog::init(plog::debug, "Logfile.txt");
-    PLOGD << "main called";
-
-    httplib::Client cli("https://www.strava.com");
-    std::string accessToken {getValidAccessToken(cli)};
-
-    httplib::Headers headers = {
-        {"Authorization", accessToken}
-    };
-
+void getAthleteStats(httplib::Client& client, httplib::Headers& headers) {
     std::ifstream inFile("strava_tokens.json");
     if (inFile) {
         json j;
         inFile >> j;
         std::string id {std::to_string(j["athlete"]["id"].get<long>())};
         std::string endpoint = {std::format("/api/v3/athletes/{}/stats", id)};
-        makeRequest(cli, headers, endpoint, true);
+        makeRequest(client, headers, endpoint, true);
     }
+}
+
+//TODO write summary at top of json with total #activities and # for each sport_type; write to file
+void athleteActivitiesToJson(httplib::Client& client, httplib::Headers& headers) {
+    int totalNumActivities {}, curPage {1};
+    int numPerPage {50};
+    std::map<std::string, int> activities;
+
+    while (true) {
+        if (curPage == 3) break; //temp for debug to avoid looking at all activities
+
+        std::string endpoint {std::format("/api/v3/athlete/activities?per_page={}&page={}", numPerPage, curPage)};
+        auto out = makeRequest(client, headers, endpoint, true);
+        if (out) {
+            json j = *out;
+            if (j.empty()) break;
+
+            curPage++;
+            totalNumActivities += j.size();
+            if (j.is_array()) {
+                for (const auto& activity : j) {
+                    std::string type {activity["sport_type"].get<std::string>()};
+                    activities[type]++;
+                }
+            }
+        }
+    }
+
+    std::cout << "total num activities: " << totalNumActivities;
+    for (const auto &[sport, count] : activities) {
+        std::cout << "sport: " << sport << " has activity count: " << count;
+    }
+}
+
+int main(int, char**){
+    plog::init(plog::debug, "Logfile.txt");
+    PLOGD << "main called";
+
+    httplib::Client client("https://www.strava.com");
+    std::string accessToken {getValidAccessToken(client)};
+
+    httplib::Headers headers = {
+        {"Authorization", accessToken}
+    };
+    athleteActivitiesToJson(client, headers);
 }
