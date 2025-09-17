4455d01bb0ed22713118b8e28b5185dd68491d2b
Irishumanoid <95321751+Irishumanoid@users.noreply.github.com>
2025-09-16 22:20:09 -0700

    find distinct routes based on athlete activities

--- src/main.cpp
+++ src/main.cpp
@@ -97,6 +97,60 @@ void getAthleteActivities(httplib::Client& client, httplib::Headers& headers, in
     }
 }
 
+
+// TODO: test and move into route_utils
+/** Gets distinct routes from json of all user activities */
+std::optional<json> getRoutes(const std::string& path) {
+    std::ifstream inFile(path);
+    PLOGD << "getRoutes called";
+    if (inFile) {
+        json j;
+        inFile >> j;
+        inFile.close();
+        PLOGD << "test";
+
+        if (j["data"].is_array()) {
+            PLOGD << "read file";
+            std::map<std::string, json> sportRoutes;
+            for (const auto& activity : j["data"]) {
+                std::string sport = activity["sport_type"];
+                std::string polyline = activity["map"]["summary_polyline"];
+                PLOGD << "found activity of type: " << sport;
+    
+                if (sportRoutes[sport].empty()) {
+                    sportRoutes[sport] = json::array();
+                    sportRoutes[sport].push_back({
+                        {"ids", json::array({activity["id"]})}, 
+                        {"polyline", polyline}});
+
+                } else {
+                    // check polyline similarity for each activity in corresponding sport's json
+                    for (const auto& other : sportRoutes[sport]) {
+                        std::string otherPolyline = other["polyline"]; 
+                        if (RouteUtils::areRoutesSame(polyline, otherPolyline)) {
+                            PLOGD << "same routes found";
+                            sportRoutes[sport]["ids"].push_back(activity["id"]);
+                            break;
+                        } else {
+                            PLOGD << "distinct routes found";
+                            sportRoutes[sport].push_back({
+                                {"ids", {activity["id"]}},
+                                {"polyline", polyline}});
+                        }
+                    }
+                }
+            }
+            json out;
+            for (const auto& [sport, data] : sportRoutes) {
+                out[sport] = data;
+            }
+            return out;
+        }
+
+    }
+    return {};
+}
+
 int main(int, char**){
     plog::init(plog::debug, "Logfile.txt");
     PLOGD << "main called";
@@ -109,8 +163,14 @@ int main(int, char**){
     };
     
     getAthlete(client, headers, "json_data/athlete_data_iris.json", true);*/
-    std::cout << std::boolalpha;
+    
+    
+    /*std::cout << std::boolalpha;
     std::string p1 {"yttaHvoviV@aDCkDBYIkDGUMKiBDQEEcD@uAF{ACsBEQQYWK[AqDDKSGEs@E}@F_@GSDkDxBo@j@aA^o@Lw@H[H[?_@MU?aCDqCG{A@u@Hc@OUO{EmAqAOiBAqE@gBBeBCuABWHc@DuAZaBt@m@NmAh@eCvASPSLUFgDhBIFe@f@kAn@k@LiBx@gAn@iGhFw@x@Y`@[p@o@nBCXFl@AH}BdIMr@m@tAAN_@jAm@jAq@|@]X_@P[@qAAs@Dq@GkCB_EAUAKEGKE]?_CjA_PZ}C\\}Ej@}FCQJ_@BBACOF_@Dk@A}@@@?ACKE}FGs@@yAEcGGw@Co@BiBCe@Co@?m@Ia@Cu@@cBA[E}@?aBEe@Bw@AW?c@Gc@@kBGcBGeCFoAQ}@?s@Cg@Em@@q@GaBDoAImACe@EQIeCqAiCkA}DmBsAq@A?D@_B}@a@MkBIgHCcF?gACoBYS?]CKZKHK?cAIoCCc@K]So@m@IMAYGCQg@]eBSwAU{@Sg@m@sAyMuPcC{EkAwAs@k@]OoGwAiAs@s@oAYaAGc@ImADcEPyF?_ACw@OoBa@_Bw@sAkAgAu@UMKCKBMX]x@e@r@g@`DuCxAiAlCcCjDwCl@m@HOBYGmEEe@Ak@Dw@FmCK_GMiANw@BoBGiBGsAKw@EiABgBH_BB_BJwB@sAB]AgE?cAD_ACcFC}@Be@AyPDaBAmA?iHB_AHmAEa@CaAAgEIwD@WDYBk@BK?i@KmB"};
     std::string p2 {"mttaHnqviVEmB?cNE[KO_B?a@IOuEFyBG}B[[qECi@KUUyAR_@GiDrB}@v@aAd@uCb@}@YwIB}AL}@k@gEmAwAOyQH_Dx@}DxA{DdCg@NqDtBW\\eAr@s@NgDxA_IvG_AzA}@hCH`As@vBm@tCqBtGa@`AeBnBuARcOOQi@AsAHeBhCw[d@aEfAyLXuAx@aCjAiCbAyAHk@@qPH}@McADaDGg@k@kAg@[yAy@}OyHsHgLuAaBgEyDwAaBqAmBi@yAg@yBUqCkBaPMk@Q{BIcEJkBhAwHHQbA{It@aD`@oDbAaDt@_AZ}@LCTaBTo@fBoDtF}NpAqCj@iBZkBMwADcA[kBh@q@h@sBVe@[yBJw@VYXKvF|@jEDfAR|CB~@[r@CdD?hBHiA@_@OGUBqOVkAJcBKuI]gLKwJJeGVsHx@gQp@w]\\oMVgFt@gIxEca@`TmgCpCa^pAwNDaBDC`C{UFcCN}AJo@Vm@p@cF|BoTn@qHv@aLHqBC_KO{@s@uBCm@TgCAq@Mm@w@{CcBqE_DkF_EaGqB{D{@aCgCwJuAmHm@IEMEq@eCgPcAaEmBwJc@oC_@{EOmDEwDLmEn@sID{AOcGBsANiCQKHd@@j@MjC@zCJlBW`I_@hFCpCDzDLlCNzBd@nDpBrJdAvFlB|L`@hBv@nD^v@fDzLrAzDpB`EpI|MlArCfArD`@~BAj@UpB@j@x@vCFh@IrNkAtPkAlLy@xFa@xE]|BNr@l@l@w@q@U^SdAO`BKpCiB~OqBfUoGrx@sO`jBuAnMyCfVaAzLs@`Y]tSM`Eu@fMYlK?|Ln@jXEbCI~@Yx@YZA|NCJ[BoBCyAb@s@FgCE_BWaDAkC[mBc@[PYz@Bj@V|AgArCi@v@^bBAvAJhAGj@{AzEsI~S{@|A[|@]bBg@fAc@h@e@|@{@nDW|Ci@rCaAvHUp@ObAGdAe@lDKlDLvDlA|IlApLXvAx@zBnBhCfInIfIzLtCnA~NrHTX\\hAHn@IjAFlBK|ACtED~IGVsBrDw@hBm@vBSdAw@|IKfDYhBuAnQHXPJfAVALUDaACYTs@rGJ`BNh@LNlDErGNxBORM`BuB~@kCtDaNKs@FWp@kBl@mAzF{ErCsBtBaAnAUbBoAtDsBtEqCxEcBzBUzKQjCBvC^jD~@|@h@fBQtKPjAKxAc@lG{DrBGd@v@b@JhECLLBXB~AKfCAvFRPxB@ZRBzAEdH"};
-    std::cout << RouteUtils::areRoutesSame(p1, p2, true);
+    std::cout << RouteUtils::areRoutesSame(p1, p2, true);*/
+    json out = getRoutes("json_data/activity_data_iris.json");
+    std::ofstream outFile ("test_distinct_routes.json");
+    outFile << out.dump(2);
+    outFile.close();
 }
--- src/route_analysis/route_utils.h
+++ src/route_analysis/route_utils.h
@@ -8,7 +8,7 @@ namespace RouteUtils {
     using Point = gepaf::PolylineEncoder<>::Point;
     std::vector<Point> parsePolylineData(const std::string& polylineString, bool verbose=false);
 
-    bool areRoutesSame(const std::string& first, const std::string& second, bool verbose, double threshold=0.8);
+    bool areRoutesSame(const std::string& first, const std::string& second, bool verbose=false, double threshold=0.8);
 }
 
 #endif
